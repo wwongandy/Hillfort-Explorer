b7b6437836c2cc900a0134746723b76e1ea23237
Andy <aw087ok@gmail.com>
2019-10-28 17:10:30 +0000

    Main Hillfort activity now uses AnkoLogger

--- app/src/main/java/org/wit/hillfortexplorer/HillfortActivity.kt
+++ app/src/main/java/org/wit/hillfortexplorer/HillfortActivity.kt
@@ -2,11 +2,14 @@ package org.wit.hillfortexplorer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
 
-class HillfortActivity : AppCompatActivity() {
+class HillfortActivity : AppCompatActivity(), AnkoLogger {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hillfort)
+        info("Hillfort Activity started..")
     }
 }
