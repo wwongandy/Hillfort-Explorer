2d55addc24482a4592fe694951dd5ba953c932ff
Andy <aw087ok@gmail.com>
2019-10-28 22:08:42 +0000

    Created Location class

--- app/src/main/java/org/wit/hillfortexplorer/models/HillfortModel.kt
+++ app/src/main/java/org/wit/hillfortexplorer/models/HillfortModel.kt
@@ -10,3 +10,10 @@ data class HillfortModel(
     var description: String = "",
     var image: String = ""
 ): Parcelable
+
+@Parcelize
+data class Location(
+    var lat: Double = 0.0,
+    var lon: Double = 0.0,
+    var zoom: Float = 0f
+) : Parcelable
