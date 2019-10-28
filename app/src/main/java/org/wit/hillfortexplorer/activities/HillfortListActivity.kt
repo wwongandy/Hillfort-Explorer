2c5a21829e54d2f6a84392a6c4cc85cefa716854
Andy <aw087ok@gmail.com>
2019-10-28 18:31:55 +0000

    Now links to hillfort creation screen upon add button press

--- app/src/main/java/org/wit/hillfortexplorer/activities/HillfortListActivity.kt
+++ app/src/main/java/org/wit/hillfortexplorer/activities/HillfortListActivity.kt
@@ -2,10 +2,7 @@ package org.wit.hillfortexplorer.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.Menu
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_hillfort_list.*
@@ -34,6 +31,13 @@ class HillfortListActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
         return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item?.itemId) {
+            R.id.item_add -> startActivityForResult<HillfortActivity(0)
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
 
