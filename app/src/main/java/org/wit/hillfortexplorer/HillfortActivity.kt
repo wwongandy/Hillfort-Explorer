ad8f3026fe0dd0e2707b267c74b6d4d1f950f1ba
Andy <aw087ok@gmail.com>
2019-10-28 17:30:32 +0000

    Basic validation for hillfort add form

--- app/src/main/java/org/wit/hillfortexplorer/HillfortActivity.kt
+++ app/src/main/java/org/wit/hillfortexplorer/HillfortActivity.kt
@@ -2,8 +2,10 @@ package org.wit.hillfortexplorer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_hillfort.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
+import org.jetbrains.anko.toast
 
 class HillfortActivity : AppCompatActivity(), AnkoLogger {
 
@@ -11,5 +13,15 @@ class HillfortActivity : AppCompatActivity(), AnkoLogger {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hillfort)
         info("Hillfort Activity started..")
+
+        btnAdd.setOnClickListener() {
+
+            val hillfortTitle = hillfortTitle.text.toString()
+            if (hillfortTitle.isNotEmpty()) {
+                info("Add button pressed, hillfort title given: $hillfortTitle")
+            } else {
+                toast("Please enter a Hillfort title")
+            }
+        }
     }
 }
