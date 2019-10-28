1ee90ced3bac8a696533e04518e982da64471292
Andy <aw087ok@gmail.com>
2019-10-28 17:08:26 +0000

    Renamed default file names

--- app/src/main/java/org/wit/hillfortexplorer/HillfortActivity.kt
+++ app/src/main/java/org/wit/hillfortexplorer/HillfortActivity.kt
@@ -3,10 +3,10 @@ package org.wit.hillfortexplorer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class HillfortActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_hillfort)
     }
 }
