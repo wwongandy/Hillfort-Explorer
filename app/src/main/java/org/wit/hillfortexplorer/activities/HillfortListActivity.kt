059d43b78bcab687c7de97d05de457414a736b08
Andy <aw087ok@gmail.com>
2019-10-28 18:15:26 +0000

    Now supports card view listing of cached hillforts

--- app/src/main/java/org/wit/hillfortexplorer/activities/HillfortListActivity.kt
+++ app/src/main/java/org/wit/hillfortexplorer/activities/HillfortListActivity.kt
@@ -2,8 +2,16 @@ package org.wit.hillfortexplorer.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_hillfort_list.*
+import kotlinx.android.synthetic.main.card_hillfort.view.*
 import org.wit.hillfortexplorer.R
 import org.wit.hillfortexplorer.main.MainApp
+import org.wit.hillfortexplorer.models.HillfortModel
 
 class HillfortListActivity : AppCompatActivity() {
 
@@ -13,5 +21,38 @@ class HillfortListActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hillfort_list)
         app = application as MainApp
+
+        val layoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = layoutManager
+        recyclerView.adapter = HillfortAdapter(app.hillforts)
+    }
+}
+
+class HillfortAdapter constructor(private var hillforts: List<HillfortModel>):
+        RecyclerView.Adapter<HillfortAdapter.MainHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainHolder {
+        return MainHolder(
+            LayoutInflater.from(parent?.context).inflate(
+                R.layout.card_hillfort,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: MainHolder, position: Int) {
+        val hillfort = hillforts[holder.adapterPosition]
+        holder.bind(hillfort)
+    }
+
+    override fun getItemCount(): Int = hillforts.size
+
+    class MainHolder constructor(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+        fun bind(hillfort: HillfortModel) {
+            itemView.hillfortTitle.text = hillfort.title
+            itemView.description.text = hillfort.description
+        }
     }
 }
