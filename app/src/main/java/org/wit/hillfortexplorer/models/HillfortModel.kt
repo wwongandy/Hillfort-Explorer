73dc135c59bf41883657deabab24eebcd52d3a81
Andy <aw087ok@gmail.com>
2019-10-28 20:44:47 +0000

    Hillfort model now uses experimental Parcelize functionality

--- app/src/main/java/org/wit/hillfortexplorer/models/HillfortModel.kt
+++ app/src/main/java/org/wit/hillfortexplorer/models/HillfortModel.kt
@@ -1,6 +1,10 @@
 package org.wit.hillfortexplorer.models
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class HillfortModel(
     var title: String = "",
     var description: String = ""
-)
+): Parcelable
