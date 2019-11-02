6a5f39f12c4a92cd17af73a7a0a315a2dae30f44
Andy <aw087ok@gmail.com>
2019-11-02 21:31:18 +0000

    Now autoremoves date visited field if visited flag is removed

--- app/src/main/java/org/wit/hillfortexplorer/models/HillfortJSONStore.kt
+++ app/src/main/java/org/wit/hillfortexplorer/models/HillfortJSONStore.kt
@@ -47,7 +47,7 @@ class HillfortJSONStore : HillfortStore, AnkoLogger {
             foundHillfort.description = hillfort.description
             foundHillfort.additionalNotes = hillfort.additionalNotes
             foundHillfort.isVisited = hillfort.isVisited
-            foundHillfort.dateVisited = hillfort.dateVisited
+            foundHillfort.dateVisited = if (foundHillfort.isVisited) { hillfort.dateVisited } else { null }
             foundHillfort.images = hillfort.images
             foundHillfort.location = hillfort.location
         }
