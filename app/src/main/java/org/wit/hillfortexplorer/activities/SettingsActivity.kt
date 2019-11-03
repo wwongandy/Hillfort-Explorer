03ee01aba78546289ce581cf5fa987096e581d78
Andy <aw087ok@gmail.com>
2019-11-03 18:22:45 +0000

    Temporary messages to handle credentials changing functionality implemented

--- app/src/main/java/org/wit/hillfortexplorer/activities/SettingsActivity.kt
+++ app/src/main/java/org/wit/hillfortexplorer/activities/SettingsActivity.kt
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_settings.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.intentFor
+import org.jetbrains.anko.toast
 import org.wit.hillfortexplorer.R
 import org.wit.hillfortexplorer.main.MainApp
 
@@ -22,6 +23,34 @@ class SettingsActivity: AppCompatActivity(), AnkoLogger {
         toolbarSettings.title = title
         setSupportActionBar(toolbarSettings)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val _username = app.currentUser.username
+        val _password = app.currentUser.password
+        changeUsername.setOnClickListener {
+            val oldUsername = oldUsername.text.toString()
+            val newUsername = newUsername.text.toString()
+
+            if (!sameCredentials(oldUsername, _username)) {
+                toast(R.string.invalid_oldUsername)
+            } else if (newUsername.isEmpty()) {
+                toast(R.string.invalid_newUsername)
+            } else {
+                toast(R.string.valid_changeUsername)
+            }
+        }
+
+        changePassword.setOnClickListener {
+            val oldPassword = oldPassword.text.toString()
+            val newPassword = newPassword.text.toString()
+
+            if (!sameCredentials(oldPassword, _password)) {
+                toast(R.string.invalid_oldPassword)
+            } else if (newPassword.isEmpty()) {
+                toast(R.string.invalid_newPassword)
+            } else {
+                toast(R.string.valid_changePassword)
+            }
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -35,5 +64,9 @@ class SettingsActivity: AppCompatActivity(), AnkoLogger {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_settings, menu)
         return super.onCreateOptionsMenu(menu)
+    }
+
+    private fun sameCredentials(first: String, second: String): Boolean {
+        return first.equals(second)
     }
 }
