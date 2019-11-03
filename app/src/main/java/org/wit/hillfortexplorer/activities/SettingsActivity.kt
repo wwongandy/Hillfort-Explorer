bf005f0c3334009ddd0c6bc58a623417ce9d9363
Andy <aw087ok@gmail.com>
2019-11-03 17:53:44 +0000

    Added functioning logout button to settings page

--- app/src/main/java/org/wit/hillfortexplorer/activities/SettingsActivity.kt
+++ app/src/main/java/org/wit/hillfortexplorer/activities/SettingsActivity.kt
@@ -1,9 +1,12 @@
 package org.wit.hillfortexplorer.activities
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_settings.*
 import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.intentFor
 import org.wit.hillfortexplorer.R
 import org.wit.hillfortexplorer.main.MainApp
 
@@ -19,5 +22,18 @@ class SettingsActivity: AppCompatActivity(), AnkoLogger {
         toolbarSettings.title = title
         setSupportActionBar(toolbarSettings)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item?.itemId) {
+            R.id.item_logout -> startActivity(intentFor<AuthenticationActivity>())
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_settings, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
