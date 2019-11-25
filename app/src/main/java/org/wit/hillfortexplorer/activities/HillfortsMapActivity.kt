0ad6f3490bc9dae912844b3aa8dcf4397dc9dca2
Andy <aw087ok@gmail.com>
2019-11-25 11:53:28 +0000

    Removed redundancies from basic activity creation

--- app/src/main/java/org/wit/hillfortexplorer/activities/HillfortsMapActivity.kt
+++ app/src/main/java/org/wit/hillfortexplorer/activities/HillfortsMapActivity.kt
@@ -12,12 +12,8 @@ class HillfortsMapActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hillforts_map)
+        toolbar.title = title
         setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
     }
 
 }
