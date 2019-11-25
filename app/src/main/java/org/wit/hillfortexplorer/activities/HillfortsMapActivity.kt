b3fe19f726abf0ef5347ffe1239144450ac8dc1f
Andy <aw087ok@gmail.com>
2019-11-25 12:03:12 +0000

    Map activity now contains google maps and card view

--- app/src/main/java/org/wit/hillfortexplorer/activities/HillfortsMapActivity.kt
+++ app/src/main/java/org/wit/hillfortexplorer/activities/HillfortsMapActivity.kt
@@ -1,19 +1,22 @@
 package org.wit.hillfortexplorer.activities
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.GoogleMap
 import org.wit.hillfortexplorer.R
 
 import kotlinx.android.synthetic.main.activity_hillforts_map.*
+import kotlinx.android.synthetic.main.content_hillforts_map.*
 
 class HillfortsMapActivity : AppCompatActivity() {
+
+    lateinit var map: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hillforts_map)
         toolbar.title = title
         setSupportActionBar(toolbar)
+        mapView.onCreate(savedInstanceState)
     }
-
 }
