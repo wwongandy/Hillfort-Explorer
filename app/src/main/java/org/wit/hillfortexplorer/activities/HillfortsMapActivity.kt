ebcb2f182e0936f233bb3c0e129e83fc1297c9d5
Andy <aw087ok@gmail.com>
2019-11-25 11:45:33 +0000

    Introduced new basic activity for handling map of hillforts

---
+++ app/src/main/java/org/wit/hillfortexplorer/activities/HillfortsMapActivity.kt
@@ -0,0 +1,23 @@
+package org.wit.hillfortexplorer.activities
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import org.wit.hillfortexplorer.R
+
+import kotlinx.android.synthetic.main.activity_hillforts_map.*
+
+class HillfortsMapActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hillforts_map)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+}
