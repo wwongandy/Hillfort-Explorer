19f8f03f52d70e7dbec67406c1123e6fade1aab9
Andy <aw087ok@gmail.com>
2020-01-03 18:50:33 +0000

    Added save button to location selection view

--- app/src/main/java/org/wit/hillfortexplorer/views/editlocation/EditLocationView.kt
+++ app/src/main/java/org/wit/hillfortexplorer/views/editlocation/EditLocationView.kt
@@ -2,18 +2,18 @@ package org.wit.hillfortexplorer.views.editlocation
 
 import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
-import kotlinx.android.synthetic.main.activity_hillfort_list.*
+import kotlinx.android.synthetic.main.activity_edit_location.*
+import kotlinx.android.synthetic.main.activity_hillfort_list.toolbar
 import org.wit.hillfortexplorer.R
 import org.wit.hillfortexplorer.models.Location
 import org.wit.hillfortexplorer.views.BaseView
@@ -25,27 +25,20 @@ class EditLocationView : BaseView(), OnMapReadyCallback, GoogleMap.OnMarkerDragL
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_map)
+        setContentView(R.layout.activity_edit_location)
 
         init(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         location = intent.extras?.getParcelable<Location>("location")!!
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
-    }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item?.itemId) {
-            android.R.id.home -> {
-                onBackPressed()
-                return true
-            }
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync {
+            it.setOnMarkerDragListener(this)
+            it.setOnMarkerClickListener(this)
+            onMapReady(it)
         }
-
-        return super.onOptionsItemSelected(item)
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
@@ -64,8 +57,23 @@ class EditLocationView : BaseView(), OnMapReadyCallback, GoogleMap.OnMarkerDragL
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(loc, location.zoom))
     }
 
-    override fun onMarkerDrag(marker: Marker) {
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_edit_location, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item?.itemId) {
+            R.id.item_save -> returnLocationData()
+            android.R.id.home -> returnLocationData()
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onMarkerDrag(marker: Marker) {
+        lat.setText("%.6f".format(marker.position.latitude))
+        lng.setText("%.6f".format(marker.position.longitude))
     }
 
     override fun onMarkerDragEnd(marker: Marker) {
@@ -84,15 +92,35 @@ class EditLocationView : BaseView(), OnMapReadyCallback, GoogleMap.OnMarkerDragL
         return false
     }
 
-    override fun onBackPressed() {
-        returnLocationData()
-        super.onBackPressed()
-    }
-
     private fun returnLocationData() {
         val resultIntent = Intent()
         resultIntent.putExtra("location", location)
         setResult(Activity.RESULT_OK, resultIntent)
         finish()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
     }
 }
