4c45a82258c074a2aa6f1abcdb6e85755b7f48db
Andy <aw087ok@gmail.com>
2020-01-04 09:15:28 +0000

    Refactored layout file for edit location map view

--- app/src/main/java/org/wit/hillfortexplorer/views/editlocation/EditLocationView.kt
+++ app/src/main/java/org/wit/hillfortexplorer/views/editlocation/EditLocationView.kt
@@ -2,7 +2,6 @@ package org.wit.hillfortexplorer.views.editlocation
 
 import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 
@@ -25,7 +24,7 @@ class EditLocationView : BaseView(), OnMapReadyCallback, GoogleMap.OnMarkerDragL
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_map)
+        setContentView(R.layout.activity_edit_location)
 
         init(toolbar, true)
 
