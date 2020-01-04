4d5233fe0a747bc9f9e3f605de81374b87284561
Andy <aw087ok@gmail.com>
2020-01-04 09:19:56 +0000

    Edit location map view contains live lat/lon values

--- app/src/main/java/org/wit/hillfortexplorer/views/editlocation/EditLocationView.kt
+++ app/src/main/java/org/wit/hillfortexplorer/views/editlocation/EditLocationView.kt
@@ -3,6 +3,7 @@ package org.wit.hillfortexplorer.views.editlocation
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
 
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -12,7 +13,9 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.activity_edit_location.*
 import kotlinx.android.synthetic.main.activity_hillfort_list.*
+import kotlinx.android.synthetic.main.activity_hillfort_list.toolbar
 import org.wit.hillfortexplorer.R
 import org.wit.hillfortexplorer.models.Location
 import org.wit.hillfortexplorer.views.BaseView
@@ -26,18 +29,31 @@ class EditLocationView : BaseView(), OnMapReadyCallback, GoogleMap.OnMarkerDragL
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_location)
 
-        init(toolbar, true)
+        super.init(toolbar, true)
 
         location = intent.extras?.getParcelable<Location>("location")!!
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
+
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync {
+            it.setOnMarkerDragListener(this)
+            it.setOnMarkerClickListener(this)
+            onMapReady(it)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_edit_location, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item?.itemId) {
             android.R.id.home -> {
+                onBackPressed()
+                return true
+            }
+
+            R.id.item_save -> {
                 onBackPressed()
                 return true
             }
@@ -63,7 +79,8 @@ class EditLocationView : BaseView(), OnMapReadyCallback, GoogleMap.OnMarkerDragL
     }
 
     override fun onMarkerDrag(marker: Marker) {
-
+        lat.setText("%.6f".format(marker.position.latitude))
+        lng.setText("%.6f".format(marker.position.longitude))
     }
 
     override fun onMarkerDragEnd(marker: Marker) {
@@ -92,5 +109,30 @@ class EditLocationView : BaseView(), OnMapReadyCallback, GoogleMap.OnMarkerDragL
         resultIntent.putExtra("location", location)
         setResult(Activity.RESULT_OK, resultIntent)
         finish()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
     }
 }
