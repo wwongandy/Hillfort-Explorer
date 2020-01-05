5b99865f35df1e5e014f24c57150588c0d6029fa
Andy <aw087ok@gmail.com>
2020-01-05 09:17:26 +0000

    Added necessary annotations to Hillfort model

--- app/src/main/java/org/wit/hillfortexplorer/models/HillfortModel.kt
+++ app/src/main/java/org/wit/hillfortexplorer/models/HillfortModel.kt
@@ -1,13 +1,17 @@
 package org.wit.hillfortexplorer.models
 
 import android.os.Parcelable
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.android.parcel.Parcelize
 import java.util.*
 import kotlin.collections.ArrayList
 
 @Parcelize
+@Entity
 data class HillfortModel(
-    var id: Long = 0,
+    @PrimaryKey(autoGenerate = true) var id: Long = 0,
     var fbId: String = "",
     var title: String = "",
     var description: String = "",
@@ -17,10 +21,12 @@ data class HillfortModel(
     var isVisited: Boolean = false,
     var dateVisited: Date? = null,
     var images: List<String> = ArrayList(),
+    @Embedded
     var location: Location = Location()
 ): Parcelable
 
 @Parcelize
+@Entity
 data class Location(
     var lat: Double = 0.0,
     var lng: Double = 0.0,
